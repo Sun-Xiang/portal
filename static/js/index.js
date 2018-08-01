eaaf299ad37cd859e527e1e656bce44c99738636
mingcen <mingcen320@163.com>
2018-08-01 16:12:07 +0800

    refs 0801

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.classificate-more').on('click',function(){
+        $('.hot-classificate-box2').css('display','block');
+        $('.classificate-less').css('display','block');
+        $('.classificate-more').css('display','none');
+    })
+})
